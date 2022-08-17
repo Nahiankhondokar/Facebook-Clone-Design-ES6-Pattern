fa65fd0c7b9d63ad483c97cabce1b01a285ddf80
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-08-17 23:27:00 +0600

    facebook login or registration design done

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import { BsPlusSquare } from "react-icons/bs";
+import Register from '../Register/Register';
+import './Login.scss'; 
+
+const Login = () => {
+
+  // modal state
+  const [regModal, setRegModal] = useState(false);
+
+  // modal show
+  const handleRegModalShow = () => {
+    setRegModal(true);
+  }
+
+  // modal hide
+  const handleRegModalHide = () => {
+    setRegModal(false);
+  }
+ 
+
+  return (
+    <>
+    <Register regModal={regModal} handleRegModalHide={handleRegModalHide} />
+    <div className='login-container'>
+      <div className="login-wrapper">
+        {/* left side */}
+        <div className="login-wrapper-left">
+          <img className='fb-logo' src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="" />
+          <span className='fb-logo-desc'>Facebook helps you connect and share with the people in your life.</span>
+        </div>
+
+        {/* right side */}
+        <div className="login-wrapper-right">
+          <div className="login-form-area shadow">
+              <form action="" className='login-form'>
+                <input className='login-form-input' type="text" placeholder='Email address or  phone number'/>
+                <input className='login-form-input'  type="text" placeholder='Password'/>
+                <button type='submit' className='login-form-sbmt-btn'>Log In</button>
+              </form>
+              <a href='#' className='forgot-pass'>Forgotten password ?
+              </a>
+              <hr />
+              <div className="create-new-acc-btn-area">
+                <button type="button" onClick={ handleRegModalShow } className='new-acc-btn'>Create New Account</button>
+              </div>
+          </div>
+
+            <p className='create-page'><a href='#'>Create a Page</a> for a celebrity, brand or business.</p>
+
+        </div>
+
+
+      </div>
+
+    </div>
+
+    
+                {/* Footer */}
+    <div className="login-footer-area">
+
+    <div className="login-wrapper">
+      <div className="login-footer-one">
+        <ul>
+          <li>English (UK) </li>
+          <li>বাংলা</li>
+          <li>অসমীয়া</li>
+          <li>हिन्दी</li>
+          <li>नेपाली</li>
+          <li>Bahasa Indonesia</li>
+          <li>العربية</li>
+          <li>中文(简体)</li>
+          <li>Bahasa Melayu</li>
+          <li>Español</li>
+          <li>Português (Brasil)</li>
+          <li className='plus-icos'><BsPlusSquare /></li>
+        </ul>
+        
+      </div>
+      <hr className='footer-hr'/>
+      <div className="login-footer-two">
+        <ul>
+          <li>Sign UP</li>
+          <li>LogIn</li>
+          <li>Messenger Lite</li>
+          <li>Facebook</li>
+          <li>Watch</li>
+          <li>Places</li>
+          <li>Games</li>
+          <li>Marketplace</li>
+          <li>Pay</li>
+          <li>Oculus</li>
+          <li>Portal</li>
+          <li>Instagram</li>
+          <li>Bulletin</li>
+          <li>Local</li>
+          <li>Fundraisers</li>
+          <li>ServicesVoting</li>
+          <li>Information</li>
+          <li>Centre</li> <br />
+          <li>Groups</li>
+          <li>About</li>
+          <li>Createad</li>
+          <li>CreatePage</li>
+          <li>Developers</li>
+          <li>Careers</li>
+          <li>Privacy</li>
+          <li>Cookies</li>
+          <li>AdChoices</li>
+          <li>Terms</li>
+          <li>Help</li>
+          <li>Contact</li>
+          <li>uploading</li>
+          <li>and non-users</li>
+        </ul>
+      </div>
+      <div className="login-footer-three">
+        <ul>
+          <li>Pay Meta © 2022 </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+    </>
+  )
+}
+
+export default Login;
